b46cd36f2e76f99688aeaebd3d773a1f1a8f01f5
김경건 <kkjin722@gmail.com>
2025-04-14 22:10:12 +0900

    feat: initial commit

---
+++ newcode/os01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void user_program(){
+
+    printf("계산중이다. 1 + 1\n", 1 + 1);
+    printf("I/O 대기중\n");
+
+
+    printf("다시 계산을 이어서 한다.");
+    
+}
---
+++ newcode/simplepipe.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+// ipc는 프로세스끼리 서로 정보를 주고 받는 것이다. 
+
+
+int main(){
+
+
+    return 0;
+}
