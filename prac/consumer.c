689ab9dfe569bf52c8850dcc77a7bcf643a0ab1f
김경건 <kkjin722@gmail.com>
2025-04-17 08:41:04 +0900

    Feat: pipe

--- prac/consumer.c
+++ prac/consumer.c
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+// 컨수머
 int main() {
     const char *name = "OS";
     const int SIZE = 4096;
@@ -11,18 +13,21 @@ int main() {
     void *ptr;
 
     // 1) (공유 메모리 열기)
-    shm_fd = /* ________________ */;
+    shm_fd = shm_open(name, O_RDONLY, 0666);
     if (shm_fd == -1) { perror("shm_open"); exit(1); }
 
     // 2) (메모리 매핑)
-    ptr = /* ________________ */;
+    ptr = mmap(0, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);
     if (ptr == MAP_FAILED) { perror("mmap"); exit(1); }
 
     // 3) (내용 읽어 출력)
-    printf("%s\n", /* ________________ */);
+    printf("%s\n", (char *)ptr);
 
     // 4) (공유 메모리 제거)
-    /* ________________ */;
+    if(shm_unlink(name)){
+        perror("Ther is an error to eleive ");
+        exit:(-1);
+    }
 
     return 0;
 }
